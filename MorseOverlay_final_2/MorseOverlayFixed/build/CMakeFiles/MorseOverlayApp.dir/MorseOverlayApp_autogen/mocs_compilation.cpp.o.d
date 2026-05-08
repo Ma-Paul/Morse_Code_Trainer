40d45a4eb4ba07e72ecfe0fc3dd2deef297a7cca
Paul M. <paul.mantz127@gmail.com>
2026-05-08 15:30:21 +0200

    Delete MorseOverlay_final_2 directory, because it's useless

--- MorseOverlay_final_2/MorseOverlayFixed/build/CMakeFiles/MorseOverlayApp.dir/MorseOverlayApp_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MorseOverlayApp.dir/MorseOverlayApp_autogen/mocs_compilation.cpp.o: \
-  /Users/paulmantz/git/Morse_Code_Trainer/MorseOverlay_final_2/MorseOverlayFixed/build/MorseOverlayApp_autogen/mocs_compilation.cpp
--- MorseOverlay_final_2/MorseOverlayFixed/build/CMakeFiles/MorseOverlayApp_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for MorseOverlayApp_autogen.
-# This may be replaced when dependencies are built.
