01e97c29d0394d8b956b50f00c9c1e13448f1536
ma-paul <no_need_to_know@youre_a_stalker.goaway>
2026-06-20 20:10:49 +0200

    Added first support for letter mode

--- mainandersrum.py
+++ mainandersrum.py
@@ -1,40 +1,43 @@
 import time
 import re
-x = 0
-eingang = input()
-morseback = {
-    "._": "a",
-    "_...": "b",
-    "_._.": "c",
-    "_..": "d",
-    ".": "e",
-    ".._.": "f",
-    "__.": "g",
-    "....": "h",
-    "..": "i",
-    ".___": "j",
-    "_._": "k",
-    "._..": "l",
-    "__": "m",
-    "_.": "n",
-    "___": "o",
-    ".__.": "p",
-    "__._": "q",
-    "._.": "r",
-    "...": "s",
-    "_": "t",
-    ".._": "u",
-    "..._": "v",
-    ".__": "w",
-    "_.._": "x",
-    "_.__": "y",
-    "__..": "z",
-}
-ausgabe = ""
-woerter = re.split(r"   ", eingang)
-for anzahl in woerter:
-    buchstaben = re.split(r" ", anzahl)
-    for test in buchstaben:
-        ausgabe += morseback[test]
-    ausgabe += " "
-print(ausgabe)
+
+
+def translateintoascii(code):
+    x = 0
+    eingang = code
+    morseback = {
+        "._": "a",
+        "_...": "b",
+        "_._.": "c",
+        "_..": "d",
+        ".": "e",
+        ".._.": "f",
+        "__.": "g",
+        "....": "h",
+        "..": "i",
+        ".___": "j",
+        "_._": "k",
+        "._..": "l",
+        "__": "m",
+        "_.": "n",
+        "___": "o",
+        ".__.": "p",
+        "__._": "q",
+        "._.": "r",
+        "...": "s",
+        "_": "t",
+        ".._": "u",
+        "..._": "v",
+        ".__": "w",
+        "_.._": "x",
+        "_.__": "y",
+        "__..": "z",
+    }
+    ausgabe = ""
+    woerter = re.split(r"   ", eingang)
+    for anzahl in woerter:
+        buchstaben = re.split(r" ", anzahl)
+        for test in buchstaben:
+            ausgabe += morseback[test]
+        ausgabe += " "
+    return ausgabe
